54efef07be6a1f809ec9410e334ea4704c9a140e
Adam Croissant <adamc41@uw.edu>
2014-01-17 00:21:39 -0800

    wrote test for overloading

---
+++ SamplePrograms/SampleMiniJavaPrograms/cse401c_ctfail_16.java
@@ -0,0 +1,16 @@
+// tests to make sure minijava compiler doesnt allow overloading
+class cse401c_ctfail_16 {
+    public static void main(String[] args) {
+        System.out.println(new TestOverload().a(0));
+    }
+}
+
+class TestOverload {
+    public int a() {
+        return 1;
+    }
+
+    public int a(int x) {
+        return x;
+    }
+}
