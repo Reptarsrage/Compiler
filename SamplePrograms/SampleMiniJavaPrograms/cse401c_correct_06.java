d89d7a4c7b015410bd0c954fdb97c6333b8cab7d
Adam Croissant <adamc41@uw.edu>
2014-01-16 23:26:38 -0800

    wrote test for function dispatch

---
+++ SamplePrograms/SampleMiniJavaPrograms/cse401c_correct_06.java
@@ -0,0 +1,12 @@
+// tests basic function dispatch
+class cse401c_correct_06 {
+    public static void main(String[] args) {
+        System.out.println(new TestFunctionDispatch().test());
+    }
+}
+
+class TestFunctionDispatch {
+    public test() {
+        return 1;
+    }
+}
