ec822fb06097fec0e5a3e1d32ec46d8b9552f013
Adam Croissant <adamc41@uw.edu>
2014-01-30 20:51:13 -0800

    modified test 08 to check mainclass with multiple statements

--- SamplePrograms/SampleMiniJavaPrograms/cse401c_correct_08.java
+++ SamplePrograms/SampleMiniJavaPrograms/cse401c_correct_08.java
@@ -2,13 +2,7 @@
 
 class cse401c_correct_08 {
 	public static void main(String[] args) {
-            System.out.println(new TestDoubleSimple().test());
-	}
-}
-
-class TestDoubleSimple {
-	public double  test() {
-		System.out.println(1.0);
+        System.out.println(1.0);
 		System.out.println(1e00);
 		System.out.println(10e-1);
 		System.out.println(1e+00);
@@ -18,6 +12,14 @@ class TestDoubleSimple {
 		System.out.println(1D);
 		System.out.println(1.01d);
 		System.out.println(1E000004);
-                return 1.0;
+
+
+        //    System.out.println(new TestDoubleSimple().test());
 	}
 }
+
+//class TestDoubleSimple {
+//	public double  test() {
+//                return 1.0;
+//	}
+//}
