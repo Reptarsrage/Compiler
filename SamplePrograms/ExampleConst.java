2ffee80efd2964f10434e17707fbfe3d38cec871
Justin Robb <xreptarx@attu2.cs.washington.edu>
2014-02-10 01:38:57 -0800

    Added short circuit or. Done with control flow section.

--- SamplePrograms/ExampleConst.java
+++ SamplePrograms/ExampleConst.java
@@ -1,26 +1,20 @@
 class ExampleConst {
   public static void main(String[] a) {
-    while (1 > 3){
-		display(6);
-		if (2 == 3) {
-		display(1 + 5);
-		display(7 - 1);
-		display(2 * 3);
-		display(18 / 3);
-		display(27 % 7);
-		} else {
-		if (2 == 6){
-		display(23 > 8);
-		display(8 < 23);
-		display(8 <= 8);
-		display(9 >= 9);
-		display(10 == 10);
-		display(11 != 10);
-		display(!(12 == 11));
-		display((12 == 12) && (5 > 2));
-		display((5 != 4) || (6 != 6));
-		} else {}
-		}
-     }
+	display(6);
+	display(1 + 5);
+	display(7 - 1);
+	display(2 * 3);
+	display(18 / 3);
+	display(27 % 7);
+	display(23 > 8);
+	display(8 < 23);
+	display(8 <= 8);
+	display(9 >= 9);
+	display(10 == 10);
+	display(11 != 10);
+	display(!(12 == 11));
+	display((12 == 12) && (5 > 2));
+	display((5 != 4) || (6 != 6));
+	display((5 <= 4) || (6 == 6));
   }
 }
