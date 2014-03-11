43a4c00245211777bc05951916bbe5b7058356cb
Adam Croissant <adamc41@uw.edu>
2014-03-11 16:36:05 -0700

    fixed issue with lack of filename when running codegen or build-example causing errors

--- src/CodeGenMain.java
+++ src/CodeGenMain.java
@@ -13,56 +13,56 @@ import java.util.*;
  * entry point, and feel free to modify it as you see fit.
  */
 public class CodeGenMain {
-  public static void main(String[] args) {
-    String inputFileName = null;
+    public static void main(String[] args) {
+	String inputFileName = null;
 	boolean countLines = false;
 	for (int i = 0; i < args.length; i++) {
-	  if (args[i].equals("-LC")) {
-        countLines = true;
-	  } else if (args[i].equals("-i")) {
-        inputFileName = args[i+1];
-        i += 1;
-      }
+	    if (args[i].equals("-LC")) {
+		countLines = true;
+	    } else if (args[i].equals("-i")) {
+		inputFileName = args[i+1];
+		i += 1;
+	    }
 	}
-	
+	if (inputFileName == null) inputFileName = "SamplePrograms/ExampleConst.java";
 	try {
 	  
-	  //
-      // create a scanner on the input file
-      //
-      scanner s = new scanner(System.in);
-      parser p = new parser(s);
-      // print to stdout
-      CodeGenerator cg = new CodeGenerator(null);
-	  TypeChecker tc = new TypeChecker();
-      Symbol root;
-      //
-      // replace p.parse() with p.debug_parse() in next line to see trace of
-      // parser shift/reduce actions during parse
-      //
-      root = p.parse();
-      Program program = (Program) root.value;
-	  program.accept(new InitialTypeVisitor(tc));
-      program.accept(new SecondaryTypeVisitor(tc));
-      program.accept(new TertiaryTypeVisitor(tc));
-      program.accept(new TypeCheckerVisitor(tc));
-      program.accept(new CodeGeneratorVisitor(tc, cg, countLines));
-	  program.accept(new CodeGeneratorInheritanceVisitor(tc));
-	  program.accept(new CodeGeneratorSecondaryVisitor(cg, tc, countLines, inputFileName));
-	  //
-      // System.out.print("\n" + "Parsing completed");
-      //
-    } catch (Exception e) {
-      //
-      // yuck: some kind of error in the compiler implementation
-      // that we're not expecting (a bug!)
-      //
-      System.err.println("Unexpected internal compiler error: " + e.toString());
+	    //
+	    // create a scanner on the input file
+	    //
+	    scanner s = new scanner(System.in);
+	    parser p = new parser(s);
+	    // print to stdout
+	    CodeGenerator cg = new CodeGenerator(null);
+	    TypeChecker tc = new TypeChecker();
+	    Symbol root;
+	    //
+	    // replace p.parse() with p.debug_parse() in next line to see trace of
+	    // parser shift/reduce actions during parse
+	    //
+	    root = p.parse();
+	    Program program = (Program) root.value;
+	    program.accept(new InitialTypeVisitor(tc));
+	    program.accept(new SecondaryTypeVisitor(tc));
+	    program.accept(new TertiaryTypeVisitor(tc));
+	    program.accept(new TypeCheckerVisitor(tc));
+	    program.accept(new CodeGeneratorVisitor(tc, cg, countLines));
+	    program.accept(new CodeGeneratorInheritanceVisitor(tc));
+	    program.accept(new CodeGeneratorSecondaryVisitor(cg, tc, countLines, inputFileName));
+	    //
+	    // System.out.print("\n" + "Parsing completed");
+	    //
+	} catch (Exception e) {
+	    //
+	    // yuck: some kind of error in the compiler implementation
+	    // that we're not expecting (a bug!)
+	    //
+	    System.err.println("Unexpected internal compiler error: " + e.toString());
 
-      //
-      // print out a stack dump
-      //
-      e.printStackTrace();
+	    //
+	    // print out a stack dump
+	    //
+	    e.printStackTrace();
+	}
     }
-  }
 }
